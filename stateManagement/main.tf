9cb18f95f7fb3d493bb54f0c091037afed7d8f69
coderakr <139627558+coderakr@users.noreply.github.com>
2026-09-15 22:27:53 +0530

    terraform state backend

---
+++ stateManagement/main.tf
@@ -0,0 +1,3 @@
+resource "aws_s3_bucket" "test" {
+  bucket = "coderakr-terraform-test-bucket"
+}
---
+++ stateManagement/versions.tf
@@ -0,0 +1,15 @@
+terraform {
+  backend "s3" {
+    bucket       = "coderakr-terraform-state"
+    key          = "dev/terraform.tfstate"
+    region       = "ap-south-1"
+    encrypt      = true
+    use_lockfile = true
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.64.0"
+    }
+  }
+}
